1812a2a728faac27e362ca25519756d14679c276
Graham Bull <graham.bull@gmail.com>
2022-06-05 16:13:58 +0100

    Step 6: Demo the re-usability of a custom hook

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,14 +4,18 @@ import { DisplayCounterValue } from '../../components/DisplayCounterValue/Displa
 import { useCounter } from '../../hooks/useCounter';
 
 export const HomePage = () => {
-  // Step 5: Abstract our state logic into a custom hook
-  const { count, handleDecrement, handleIncrement } = useCounter();
+  // Step 6: Demo the re-usability of a custom hook
+  const counter1 = useCounter();
+  const counter2 = useCounter();
 
   return (
     <div>
       <h2>Home</h2>
-      <Counter count={count} onDecrement={handleDecrement} onIncrement={handleIncrement} />
-      <DisplayCounterValue count={count} />
+      <Counter count={counter1.count} onDecrement={counter1.handleDecrement} onIncrement={counter1.handleIncrement} />
+      <DisplayCounterValue count={counter1.count} />
+
+      <Counter count={counter2.count} onDecrement={counter2.handleDecrement} onIncrement={counter2.handleIncrement} />
+      <DisplayCounterValue count={counter2.count} />
     </div>
   );
 };
